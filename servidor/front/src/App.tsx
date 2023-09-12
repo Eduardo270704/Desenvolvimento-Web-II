f31277edc13a2f3c0c61b2814a7c83bbd7777bb3
Eduardo270704 <luis270704@gmail.com>
2023-09-11 22:50:23 -0300

    1

---
+++ servidor/front/src/App.tsx
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { useState } from "react";
+
+function App() {
+  const [name, setName] = useState("");
+  function salvar() {
+    axios.post("http://localhost:3001/marca", {name})
+    .then(({data}) => console.log(data));
+  }
+  return (
+    <div>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Digite o nome"
+      />
+      <div>oi {name}</div>
+      <button onClick={salvar}>Salvar</button>
+    </div>
+  );
+}
+
+export default App;
